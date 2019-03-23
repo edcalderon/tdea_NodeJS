488fa0ab8ef474b2e626af8ac9a7a8c3af41550b
ValentinaSabogal <48831750+ValentinaSabogal@users.noreply.github.com>
2019-03-23 15:50:21 -0500

    Add files via upload

---
+++ InscribirCursos.js
@@ -0,0 +1,42 @@
+const exportedcourses = require ('./Cursos');
+const fs = require('fs');
+
+const options = {
+   id:{
+     demand: true,
+     alias: 'i'
+   },
+   nombre:{
+     demand: true,
+     alias: 'n'
+   },
+   cedula:{
+     demand: true,
+     alias: 'c'
+   }
+}
+const argv = require('yargs')
+            .command('inscribir','inscribirse ',options)
+            .argv
+
+var infoCurso = exportedcourses.cursos.find(curso => curso.id  == argv.i );
+
+let createArchivo = () =>{
+  texto = "El estudiante "+argv.n+" con celuda  "+argv.c+ "\n" +
+           " se a matriculado en el curso  "+ infoCurso.nombre + " que tiene una duracion de "+infoCurso.duracion+
+           " horas y un valor de "+infoCurso.valor + " pesos";
+  fs.appendFile('Incripcion.txt',texto,(err)=>{
+    if (err) throw (err);
+    console.log('se ha creado el archivo');
+  });
+}
+if(!infoCurso && !argv.i){
+    exportedcourses.show();
+}
+if (!infoCurso && argv.i ){
+  console.log("Mensaje de Alerta no se encontro el id");
+  exportedcourses.show();
+}
+if (infoCurso){
+  createArchivo();
+}
