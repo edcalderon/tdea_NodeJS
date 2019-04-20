7b3dab7b425f55e47964fdc9dd4010df49c2732e
edcalderon <edwardca12@gmail.com>
2019-04-20 02:18:13 -0500

    modified:   src/config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb://localhost:27017/app-db?ssl=true'
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/app-db';
+}
+else {
+	urlDB = 'mongodb+srv://admin:admin@appcluster-4c37k.mongodb.net/app-db?retryWrites=true'
+
+}
+
+process.env.URLDB = urlDB
