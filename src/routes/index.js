dc17ed46b0af2aed182332194343643a23ab2634
edcalderon <edwardca12@gmail.com>
2019-04-22 05:36:57 -0500

    modified:   mongodb-data/WiredTiger.turtle 	modified:   mongodb-data/WiredTiger.wt 	modified:   mongodb-data/collection-11--3017453097082409667.wt 	modified:   mongodb-data/collection-2--3017453097082409667.wt 	modified:   mongodb-data/collection-4--3017453097082409667.wt 	new file:   mongodb-data/diagnostic.data/metrics.2019-04-22T09-47-19Z-00000 	modified:   mongodb-data/diagnostic.data/metrics.interim 	modified:   mongodb-data/index-12--3017453097082409667.wt 	modified:   mongodb-data/index-13--3017453097082409667.wt 	modified:   mongodb-data/index-3--3017453097082409667.wt 	modified:   mongodb-data/index-5--3017453097082409667.wt 	modified:   mongodb-data/index-6--3017453097082409667.wt 	renamed:    mongodb-data/journal/WiredTigerLog.0000000001 -> mongodb-data/journal/WiredTigerLog.0000000002 	modified:   mongodb-data/journal/WiredTigerPreplog.0000000001 	modified:   mongodb-data/journal/WiredTigerPreplog.0000000002 	modified:   mongodb-data/mongod.lock 	modified:   mongodb-data/sizeStorer.wt 	modified:   src/routes/index.js 	modified:   templates/views/dashboarduser.hbs

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,7 @@ require('./../helpers/helpers');
 const directorio_partials = path.join(__dirname, './../../templates/partials');
 const directorio_views = path.join(__dirname, './../../templates/views');
 
+
 // HBS
 hbs.registerPartials(directorio_partials);
 app.set('views',directorio_views);
@@ -180,32 +181,74 @@ app.get('/dashboarduser', (req, res) =>{
 });
 
 app.post('/dashboarduser', (req, res) =>{
-	//********************* */Actualización*****
-	Course.findOneAndUpdate({name: req.body.inscribir},{$addToSet:{students: req.session.user}}, (err, curso) =>{
-		console.log('RESULTADOS DEL POST')
-		console.log(req.body.inscribir);
-		console.log(curso)
-		if (err){
-			return res.render('dashboarduser',{
-			  resultshow: "Hubo un error: " + err,
-			  cardcolor: "danger"
-		 })
-		}
+	//Validación
+// 	var conditions = {
+//     name: req.body.inscribir//,
+//     //students: { $ne: req.session.user}
+// };
+// 	Course.find( conditions,(err,result)=>{
+// 		if (err){
+// 			return console.log(err)
+// 		}
+ //		console.log('LOS RESULTADOS SON:')
+// 		console.log(req.session.user)
+// 		console.log(result)
+// 		let objeto = result.find(e => e.students === req.session.user);
+// 		console.log(objeto)
+// 		if(result !== []){
+// 			console.log('Te puedes inscribir')
+// 		}else{
+// 			console.log('no te puedes inscribir')
+// 		}
+// });
 
-		res.render ('dashboarduser', {
-			listado: req.session.listado,
-			name: curso.name,
-			description: curso.description,
-			value: curso.value,
-			intensity: curso.intensity,
-			modality: curso.modality,
-			state: curso.state,
-			students: req.session.user,
-			resultshow: "¡Se inscribió exitosamente en el curso " + curso.name + "!",
-			cardcolor: "success"
-	})
-})
+var conditions = {
+	name: req.body.inscribir,
+	students: { $in: req.session.user}
+};
 
+Course.find(conditions,(err,result)=>{
+	if (err){
+		return console.log(err)
+	}
+	console.log('LOS RESULTADOS SON:')
+	console.log(req.session.user)
+	console.log(result)
+	console.log(result.length)
+	if(result.length == 0){
+			//********************* Actualización*****
+			console.log('Te inscribiste correctamente!!!!')
+			Course.findOneAndUpdate({name: req.body.inscribir},{$addToSet:{students: req.session.user}}, (err, curso) =>{
+				console.log('RESULTADOS DEL POST')
+				console.log(req.body.inscribir);
+				console.log(curso)
+				if (err){
+					return res.render('dashboarduser',{
+					  resultshow: "Hubo un error: " + err,
+					  cardcolor: "danger"
+				 })
+				}
+
+				res.render ('dashboarduser', {
+					listado: req.session.listado,
+					name: curso.name,
+					description: curso.description,
+					value: curso.value,
+					intensity: curso.intensity,
+					modality: curso.modality,
+					state: curso.state,
+					students: req.session.user,
+					resultshow: "¡Se inscribió exitosamente en el curso " + curso.name + "!",
+					cardcolor: "success"
+				})
+			})
+	}else{
+		return res.render('dashboarduser',{
+			resultshow: "Upss! Actualmente estas inscrito",
+			cardcolor: "warning"
+		})
+	}
+});
 });
 
 app.get('/register', (req, res) =>{
@@ -240,7 +283,7 @@ app.post('/register', (req, res) =>{
 
 app.get('/dashboardadmin', (req, res) =>{
   	res.render('dashboardadmin', {
-			verCursosDisponibles: req.query.verCursosDisponibles
+
 		})
 });
 
@@ -264,6 +307,8 @@ app.post('/dashboardadmin', (req, res) =>{
 								})
 			 }
 			 return res.render('dashboardadmin',{
+
+
 				 			result: "Hecho!",
 							resultshow: "Curso creado correctamente",
 							cardcolor: "success"
